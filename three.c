33fbb455077ccef6a97a0a08ef7bbad1f044170c
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-26 13:53:34 +0530

    Add files via upload
    check divisible by 5

---
+++ three.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num%5==0){
+        printf("number is divisible by 5");
+    }
+    else{
+        printf("number is not divisible 5");
+    }
+    return 0;
+
+}
