b4037773837a2ab211b6dbf02ede474f3117a8b5
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-27 22:23:22 +0530

    Add files via upload
    System Access Control

---
+++ sw20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int role, time;
+    scanf("%d", &role);
+    scanf("%d", &time);
+    switch (role) {
+        case 1:
+            printf("Full Access\n");
+            break;
+        case 2:
+            if (time >= 9 && time <= 18) {
+                printf("Limited Access\n");
+            } else {
+                printf("Access Denied\n");
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
