884bc5de374fd05b20c83361be2262d00a9a3c92
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-28 16:54:49 +0530

    Add files via upload
    Fibonacci Series

---
+++ f10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a=0;
+    int b=1;
+    int c=a+b;
+    printf("%d %d",a,b);
+    for(int i=3;i<=n;++i){
+        printf(" %d ",c);
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    
+    return 0;
+}
