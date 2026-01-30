76c220f528297e91ecab9e047ad8281f8e782885
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-30 19:12:16 +0530

    Add files via upload
    print pattern program

---
+++ pattern1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int n=1;n<=5-i;n++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=5;i>0;i--){
+        for(int n=1;n<=5-i;n++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
