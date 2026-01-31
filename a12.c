e8a096aa11d43893d50de4eb1dad515ae69e5342
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-31 21:06:06 +0530

    Add files via upload
    Find Two Products Matching Bill Amount

---
+++ a12.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d", &size); 
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    int target;
+    scanf("%d", &target);
+    int found = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            if(arr[i] + arr[j] == target){
+                printf("%d %d\n", arr[i], arr[j]);
+                found = 1;
+                break;
+            }
+            
+        }
+        if (found) {
+            break;
+        }
+    }
+    if (!found) {
+        printf("No Pair\n");
+    }
+
+    return 0;
+}
