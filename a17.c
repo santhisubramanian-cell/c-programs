7f2f718e07b730f2ccd3852092ddc44755494a88
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-01-31 21:12:34 +0530

    Add files via upload
    Check Palindrome Sensor Data

---
+++ a17.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    int left = arr[0];
+    int right =arr[size - 1] ;
+    while(left < right){
+        if(left!=right){
+            printf("no");
+        }
+        else{
+            printf("yes");
+        }
+        left++;
+        right--;
+        
+    }
+    
+    return 0;
+
+}
+
+
+   
+
