dfa128f26b364c18fa149f0eef0577b4ba8e291f
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-02 20:29:00 +0530

    Add files via upload
    Find minimum element in an array

---
+++ a29.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int min=a[0];
+    for(int j=0;j<n;j++){
+        if(a[j]<min){
+            min=a[j];
+        }
+    }
+    printf("minimum: %d",min);
+    return 0;
+}
