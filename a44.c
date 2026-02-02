67e043d92d3ef84713cb29df77e284821db9d7ec
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-02 20:43:41 +0530

    Add files via upload
    Find duplicate elements

---
+++ a44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,pos;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&pos);
+    pos--;
+    for(int i=pos;i<n-1;i++){
+        a[i]=a[i+1];
+    }
+    for(int i=0;i<n-1;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
