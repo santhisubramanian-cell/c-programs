2ac513af7722a5e2b5430c41269294b266a9147c
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-03 20:42:13 +0530

    Add files via upload
    Column with Maximum Total Performance

---
+++ array14.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int index=1;
+    int max=0;
+    for(int j=0;j<c;j++){
+        int sum=0;
+        for(int i=0;i<r;i++){
+            sum+=a[i][j];
+        }
+        if(sum>max){
+            max=sum;
+            index=j;
+        }
+    } 
+    printf("%d",index);   
+    return 0;
+}
