08e6e9316eaf135d0d04ef3f7990e52eb934893f
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-03 20:41:29 +0530

    Add files via upload
    Count Palindromic Department Records

---
+++ array13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int count = 0;
+    for(int i = 0; i < r; i++){
+        int flag=1;
+        for(int j = 0; j < c/2; j++){
+            if(a[i][j] != a[i][c-1-j]){
+                flag=0;
+                break;
+            }
+        }
+        if(flag){
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
