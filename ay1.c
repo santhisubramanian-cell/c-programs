184826cc83732b82c603730d3e970ebac2cec7fa
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:25:56 +0530

    Add files via upload

---
+++ ay1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int s;
+    scanf("%d",&s);
+    int a[s];
+    for(int i=0;i<s;i++){
+        scanf("%d",&a[i]);
+    }
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int temp=a[0];
+        for(int j=0;j<s;j++){
+            a[j]=a[j+1];
+        }
+        a[s-1]=temp;
+    }
+    for(int i=0;i<s;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
