e97f579fa2a277305036bae087f28248de3f3019
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:45:15 +0530

    Add files via upload

---
+++ td15.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int a[r][c],b[r][c];
+    printf("enter a array");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("enter array b");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            a[i][j]-=b[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
