ff62c5cd7bea185fda37c4b0ab2511a21043a6e4
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:46:38 +0530

    Add files via upload

---
+++ td18.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int sum;
+    int min=a[0][0];
+    for(int j=0;j<c;j++){
+        sum=0;
+        for(int i=0;i<r;i++){
+            sum+=a[j][i];
+        }
+        printf("%d",sum);
+    }
+
+
+    return 0;
+}
