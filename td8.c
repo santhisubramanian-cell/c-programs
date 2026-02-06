43556204b2c691e609b2a49be57b99d171619373
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:42:39 +0530

    Add files via upload

---
+++ td8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int r,c,found=0;
+    scanf("%d %d",&r,&c);
+    if(r==c){
+        printf("it is a square matrix");
+    }
+    else{
+        printf("it is not a square matrix");
+    }
+    return 0;
+
+
+}
