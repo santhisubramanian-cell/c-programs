399fb8d57646776da7a6c1fd2cef821984d0c7af
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:29:00 +0530

    Add files via upload

--- p13.c
+++ p13.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int main(){
-    int n=1;
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            printf("%d ",n);
-            n++;
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==n-2 || j==n-2){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
         }
-        
-        
         printf("\n");
-        
     }
     return 0;
 }
