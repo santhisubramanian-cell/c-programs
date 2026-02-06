5bea870c3a7e4ecfc25c96819821874b728ae3c3
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:32:39 +0530

    Add files via upload

---
+++ pn6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        for(int s=1;s<=n-i;s++){
+            printf(" ");
+
+        }
+        for(int j=1;j<=i;j++){
+            if(i==n || j==i || j==1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
