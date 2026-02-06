e9590817253fb83aa57f6608f1f203ee5f7faaf6
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-06 19:30:12 +0530

    Add files via upload

---
+++ pn2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n-2;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1 || i==n-2 || j==n || j==1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
