2ed46c73e5070d4fb25accdfb622ef14d45938ad
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-09 20:45:03 +0530

    Add files via upload
    Space Counter in Delivery Address

---
+++ sp8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char str[200];
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==' '){
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+    return 0;
+}
