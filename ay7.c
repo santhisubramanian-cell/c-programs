71bd566b8768691f2f0db2f8bea2b9ae89906fe9
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-10 20:32:16 +0530

    Add files via upload
    find union of two arrays

---
+++ ay7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int sum;
+    scanf("%d",&sum);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]+a[j]==sum){
+                printf("%d %d",a[i],a[j]);
+            }
+        }
+    }
+
+    return 0;
+}
