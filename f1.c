b83a01ad1acb2224db9e991fc5ba373a862f67bc
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-11 13:56:38 +0530

    Add files via upload

--- f1.c
+++ f1.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        printf("%d ",i);
+int digits(char str[]){
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='0' && str[i]<='9'){
+            count++;
+        }
     }
+    return count;
+}
+    int main(){
+    char str[200];
+    fgets(str,sizeof(str),stdin);
+    int count = digits(str);
+    printf("%d",count);
     return 0;
 }
