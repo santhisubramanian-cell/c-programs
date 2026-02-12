aab711d09dea61f842f71dce7a3ef22bda9b61d3
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-02-12 15:39:42 +0530

    Add files via upload
    Count Vowels in String Using Pointers ( )

---
+++ pointers8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void isvowels(char str[]){
+    char *p=str;
+    int count=0;
+    while (*p!='\0')
+    {
+        if(*p=='a' || *p=='e' || *p=='i' || *p=='o' || *p=='u'|| *p=='A' || *p=='E' || *p=='I' || *p=='O' || *p=='U'){
+            count++;
+        }  
+        *p++;
+    }
+    
+    printf("%d",count);
+}
+
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    isvowels(str);
+    return 0;
+}
