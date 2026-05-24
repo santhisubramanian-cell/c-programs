db5e10e770919e94de0bacc57a1d16a237585860
Santhi Subramanian <santhisubramanian2006@gmail.com>
2026-05-24 23:05:34 +0530

    Fix formatting issues in a17.c

--- a17.c
+++ a17.c
@@ -3,7 +3,7 @@ int main(){
     int size;
     scanf("%d", &size);
     int arr[size];
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i<size;i++){
         scanf("%d", &arr[i]);
     }
     int left = arr[0];
